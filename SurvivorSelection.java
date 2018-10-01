a33112886b6cf28b6987ce7efa0ec816de6ad6df
Kiki van Rongen <Kiki@MacBook-Air-van-Kiki.local>
2018-10-01 11:52:44 +0200

    new survivor selection

--- SurvivorSelection.java
+++ SurvivorSelection.java
@@ -27,6 +27,8 @@ public class ParentSelection
         return replaceWorst(oldPopulation, allScores, numChild);
       case "elitism" :
         return elitism(oldPopulation, allScores, numChild);
+      case "roundRobin":
+        return roundRobin(oldPopulation, allScores, numChild);
     }
 
     // Return empty list if no case has been used
@@ -54,7 +56,7 @@ public class ParentSelection
     // remove worst individuals
     double[][] newPopulation = sortedPopulation.removeRange(0,numChild);
 
-    return newPopulation
+    return newPopulation;
 
   }
 
@@ -63,28 +65,102 @@ public class ParentSelection
   {
     int populationSize = 100;
     int nrTraits = 10;
-    double[][] newPopulation = new double[populationSize][nrTraits]
+    double[][] newPopulation = new double[populationSize][nrTraits];
 
-    // find maximum fitness value and corresponding index
+    // determine number of parents plus children
+    int length = oldPopulation.length;
+
+    // find maximum fitness value and corresponding index in population
     double bestScore = Collections.max(allScores);
-    int bestInd = oldPopulation.get(bestScore);
+    int bestIndividual = allScores.indexOf(bestScore);
 
     int[] elimIndividuals = new int[numChild];
     for (i = 0; i < numChild; i++)
     {
       // select random individual
-      int randomInd = rnd_.nextInteger(100);
+      int randomInd = rnd_.nextInteger(length);
 
       // ensure that fittest individual is not eliminated
-      while randomInd == bestInd
+      while randomInd == bestIndividual
       {
-        randomInd = rnd_.nextInteger(100);
+        randomInd = rnd_.nextInteger(length);
       }
       // select random individual to be eliminated
       elimIndividuals[i] = randomInd;
     }
 
-    // REMOVE ELIMINATED INDIVIDUALS FROM POPULATION
+    // remove eliminated individuals
+    for (i = 0; i < length; i++)
+    {
+      // add individual to new population, if not in eliminated list
+      if i != elimIndividuals[i]
+      {
+        newPopulation[i] = oldPopulation[i];
+      }
+    }
+
+    return newPopulation;
+
+  }
+
+  // evaluate each individual against others and choose new population accordingly
+  public double[][] roundRobin(double[][] oldPopulation, double[] allScores, int numChild)
+  {
+    int populationSize = 100;
+    int nrTraits = 10;
+
+    // determine number of parents plus children
+    int length = oldPopulation.length;
+
+    // store number of wins of each individual
+    int[] wins = new int[length];
+
+    for (i = 0; i < length; i++)
+    {
+      int numWins = 0;
+      int numOpponents = 0;
+
+      // choose opponents by score
+      while numOpponents < 10
+      {
+        int opp = rnd_.nextInteger(length);
+
+        // ensure that opponent and individual are not equal
+        while opp == i
+        {
+          opp = rnd_.nextInteger(length);
+        }
+
+        // determine win or no win
+        if allScores[i] > allScores[opp]
+        {
+          numWins += 1;
+        }
+
+        // select next opponent
+        numOpponents += 1
+      }
+
+      // store number of 'wins' in list
+      wins[i] = numWins;
+    }
+
+    // select individuals with most 'wins'
+    int[] sortedWins = new int[length];
+    sortedWins = Arrays.sort(wins);
+
+    double[][] sortedPopulation = new double[populationSize + numChild][nrTraits];
+
+    for (j = 0; j < length; j++)
+    {
+      int idx = wins.indexOf(sortedWins[j]); // zijn er niet te veel individuen met evenveel 'wins'???
+      sortedPopulation[j] = oldPopulation[idx];
+    }
+
+    // remove individuals with fewest 'wins'
+    double[][] newPopulation = sortedPopulation.removeRange(0,numChild);
+
+    return newPopulation;
 
   }
 
