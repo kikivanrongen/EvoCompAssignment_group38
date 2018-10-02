e22f6e69b3da52f014dea072b9d8c877c9713a5e
kimdebie <kimdebie@outlook.com>
2018-10-02 17:15:23 +0200

    edited runner

--- runner.sh
+++ runner.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-javac -cp contest.jar player38.java ParentSelection.java Recombination.java SurvivorSelection.java
-jar cmf MainClass.txt submission.jar player38.class ParentSelection.class Recombination.class SurvivorSelection.class
+javac -cp contest.jar player38.java ParentSelection.java Recombination.java #SurvivorSelection.java
+jar cmf MainClass.txt submission.jar player38.class ParentSelection.class Recombination.class #SurvivorSelection.class
 
 echo "Start sphere evaluation"
 
@@ -12,3 +12,9 @@ java -jar testrun.jar -submission=player38 -evaluation=SphereEvaluation -seed=1 
 # java -jar testrun.jar -submission=player38 -evaluation=SchaffersEvaluation -seed=1 >> results.txt
 # echo "Katsuura Evaluation Function: " >> results.txt
 # java -jar testrun.jar -submission=player38 -evaluation=KatsuuraEvaluation -seed=1 >> results.txt
+
+
+
+
+jar cmf MainClass.txt submission.jar *.class #ParentSelection.class Recombination.class #SurvivorSelection.class
+java -jar testrun.jar -submission=player38 -evaluation=SphereEvaluation -seed=1
