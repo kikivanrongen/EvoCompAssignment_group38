1833f05f6bb06a2634453c2f8323ec74baa436d7
Nigel van Herwijnen <nigelvanherwijnen@chello.nl>
2018-10-20 15:29:40 +0200

    heatmap

---
+++ data_parameter_search/view_data.py
@@ -0,0 +1,74 @@
+import matplotlib
+matplotlib.use("TkAgg")
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+import numpy as np
+import scipy as sp
+import csv
+import sys
+
+# Define all filenames data is stored in
+filenames_c_asga = []
+filenames_c_aw1 = []
+filenames_s = []
+filenames_k_a11 = []
+filenames_k_a1ga = []
+
+for i in range(15):
+    filenames_c_asga.append("data_parameter_search/cigar_as_ga_" + str(i) + ".txt")
+    filenames_c_aw1.append("data_parameter_search/cigar_aw_1_" + str(i) + ".txt")
+    filenames_s.append("data_parameter_search/schaffers_bl_un_" + str(i) + ".txt")
+    filenames_k_a11.append("data_parameter_search/katsuura_a1_1_" + str(i) + ".txt")
+    filenames_k_a1ga.append("data_parameter_search/katsuura_a1_ga_" + str(i) + ".txt")
+
+
+# Define variable data will be stored in (mean/std, recombination, mutation)
+data = np.ndarray((5, 2, 5, 3))
+
+for files, filenames in enumerate([filenames_c_asga,
+                                    filenames_c_aw1,
+                                    filenames_s,
+                                    filenames_k_a11,
+                                    filenames_k_a1ga]):
+    # Load data from cigar files
+    for i, item in enumerate(filenames):
+
+        # Open file
+        with open(item, "r") as csvfile:
+            score = []
+            spamreader = csv.reader(csvfile, delimiter="\n")
+            for j, row in enumerate(spamreader):
+                if j % 2 == 0:
+                    score.append(float(row[0][7::]))
+
+            score = np.array(score)
+
+            print(item, np.mean(score))
+
+            # print(score)
+            data[files, 0, i%5, i//5] = np.mean(score)
+            data[files, 1, i%5, i//5] = sp.stats.sem(score)
+
+
+sns.set(font_scale = 1.2)
+
+y_labels = ["0.01", "0.05", "0.1", "0.2", "0.5"]
+x_labels = ["0.1", "0.3", "0.5"]
+titles = ["Cigar as-ga", "Cigar aw-1", "Schaffers bl-un", "Katsuura a1-1", "Katsuura a1-ga"]
+
+titles = ["Cigar as-ga", "Cigar aw-1", "Schaffers bl-un", "Katsuura a1-1", "Katsuura a1-ga"]
+f, axes = plt.subplots(1, 5, figsize=(12, 3), sharex=True, sharey=True)
+# sns.set_context("notebook", rc={"axes.labelsize":36})
+for i in range(5):
+    sns.heatmap(data[i,0], annot=True, vmin=0, vmax=10, ax=axes[i],
+                    xticklabels=x_labels, yticklabels=y_labels,
+                    cbar=False if i != 4 else True)
+    if i == 0:
+        axes[i].set(ylabel=r"$\sigma$")
+
+    axes[i].set(title=titles[i], xlabel=r"$\alpha$")
+    axes[i].invert_yaxis()
+
+f.tight_layout()
+plt.show()
